7db0aa96c99bf690265e278741298b55b845294e
fmoise <fmoise28@stsaviour.org>
2026-05-12 11:44:27 -0400

    origin

---
+++ rpg-maker.py
@@ -0,0 +1,10 @@
+
+
+    # a mini game for fandom lovers who love rpg makers.
+    # you can loose or gain points of guessing what game you input!
+    # enjoy and hopefully you can win this game!
+    # print_dramatic_test (' ...)
+
+if __name__ == '___main___':
+    print('Welcome to guess that RPG maker!')
+    print('You will be guessing rpgs from 2000s and up to date.')
